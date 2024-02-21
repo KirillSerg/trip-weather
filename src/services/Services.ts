2e6a4efd4a1c7c5a17e8d437fdd4142c4d8dc244
KirillSerg <kirill.sergienko1989@gmail.com>
2024-02-22 01:20:53 +0200

    feat: added weather services

--- src/services/Services.ts
+++ src/services/Services.ts
@@ -2,10 +2,12 @@ import { LocationResponse } from "../types/common";
 
 const GEO_URL = import.meta.env.VITE_API_GEO_URL;
 const GEO_KEY = import.meta.env.VITE_API_GEO_KEY;
+const WEATHER_URL = import.meta.env.VITE_API_WEATHER_URL;
+const WEATHER_KEY = import.meta.env.VITE_API_WEATHER_KEY;
 
 export const getCities = async (inputValue: string) => {
   const response = await fetch(
-    `${GEO_URL}?minPopulation=10000&namePrefix=${inputValue}`,
+    `${GEO_URL}?minPopulation=10000&limit=10&namePrefix=${inputValue}`,
     {
       method: "GET",
       headers: {
@@ -24,3 +26,24 @@ export const getCities = async (inputValue: string) => {
     }),
   };
 };
+// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/38.9697,-77.385/[date1]/[date2]?key=YOUR_API_KEY
+// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[city]/[date1]/[date2]?unitGroup=metric&include=days&key=YOUR_API_KEY&contentType=json
+// https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[city]/today?unitGroup=metric&include=days&key=YOUR_API_KEY&contentType=json
+
+// "/38.9697,-77.385?include=days&key=HKDSKHGNYCSEBRB76GB6KKE9V&contentType=json"
+export const getForecast = async (location: string, period: string) => {
+  const response = await fetch(
+    `${WEATHER_URL}/${location}/${period}?unitGroup=metric&include=days&key=${WEATHER_KEY}&contentType=json`, { method: "GET" }
+  );
+  const data = await response.json();
+  return data;
+}
+
+export const getCurrentWeather = async (location: string) => {
+  const response = await fetch(
+    `${WEATHER_URL}/${location}/today?include=days&key=${WEATHER_KEY}&contentType=json`, { method: "GET" }
+  );
+  const data = await response.json();
+  console.log(data)
+  return data;
+}
--- src/types/common.ts
+++ src/types/common.ts
@@ -37,38 +37,56 @@ export interface Trip {
   img: string
 }
 
-// export Weather {
-//   "latitude" : number;
-//   "longitude" : number;
-//   "resolvedAddress" : string;
-//   "address" : string;
-//   "timezone" : string;
-//   "tzoffset" : number;
-//   "description": string;
-//   "days" : [{ //array of days of weather data objects
-//     "datetime": "2020-11-12",
-//     "datetimeEpoch": 1605157200,
-//     "temp": 59.6,
-//     "feelslike": 59.6,
-//     ...
-//     "stations" : {
-//   },
-//     "source" : "obs",
-//     "hours" : [{  //array of hours of weather data objects
-//       "datetime": "01:00:00",
-//       ...
-//          }, ...]
-//      },...],
-// "alerts" : [{
-//   "event": "Flash Flood Watch",
-//   "description": "...",
-//   ...
-//          }
-// ],
-//   "currentConditions" : {
-//   "datetime" : "2020-11-11T22:48:35",
-//     "datetimeEpoch" : 160515291500,
-//       "temp" : 67.9,
-//          ...
-// }
+// {
+//   "queryCost": 1,
+//     "latitude": 42.5,
+//       "longitude": 45.5,
+//         "resolvedAddress": "42.5,45.5",
+//           "address": "42.5,45.5",
+//             "timezone": "Asia/Tbilisi",
+//               "tzoffset": 4.0,
+//                 "days": [
+//                   {
+//                     "datetime": "2024-02-22",
+//                     "datetimeEpoch": 1708545600,
+//                     "tempmax": 19.8,
+//                     "tempmin": 4.9,
+//                     "temp": 11.3,
+//                     "feelslikemax": 11.3,
+//                     "feelslikemin": -5.6,
+//                     "feelslike": 1.9,
+//                     "dew": 8.3,
+//                     "humidity": 87.5,
+//                     "precip": 0.024,
+//                     "precipprob": 100.0,
+//                     "precipcover": 25.0,
+//                     "preciptype": [
+//                       "snow"
+//                     ],
+//                     "snow": 0.0,
+//                     "snowdepth": 16.8,
+//                     "windgust": 8.3,
+//                     "windspeed": 7.6,
+//                     "winddir": 215.7,
+//                     "pressure": 1026.6,
+//                     "cloudcover": 93.6,
+//                     "visibility": 2.3,
+//                     "solarradiation": 149.6,
+//                     "solarenergy": 12.9,
+//                     "uvindex": 6.0,
+//                     "severerisk": 10.0,
+//                     "sunrise": "07:45:41",
+//                     "sunriseEpoch": 1708573541,
+//                     "sunset": "18:38:09",
+//                     "sunsetEpoch": 1708612689,
+//                     "moonphase": 0.42,
+//                     "conditions": "Snow, Overcast",
+//                     "description": "Cloudy skies throughout the day with snow in the morning and afternoon.",
+//                     "icon": "snow",
+//                     "stations": [
+//                       "remote"
+//                     ],
+//                     "source": "comb"
+//                   }
+//                 ]
 // }
